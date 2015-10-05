48a5a4b31c547e0cece2327d5d4d0e84998616fb
jaymuhammad <muhammadjihad8@aol.com>
2015-10-05 10:22:22 -0400

    project 3
    new commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function isEmailEmpty() {
+    var field = documentGetElementById('email');
+if (field.value === '') {
+    return true;
+   } else {
+          return false
+}
+}
+var fieldTest = isEmailEmpty();
+ if (fieldTest === true) {
+    alert('Please provide your email address');
+}
+
